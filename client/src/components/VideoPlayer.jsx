382be0ffb540445f0e273cb1dfcb91c960986ab0
Affaq-Ahmed <aaawan165@gmail.com>
2024-02-03 14:14:45 +0500

    Refactor VideoPlayer component to include video elements and context

--- client/src/components/VideoPlayer.jsx
+++ client/src/components/VideoPlayer.jsx
@@ -1,9 +1,63 @@
-import React from 'react'
+import React, { useContext, useEffect } from "react";
+import { SocketContext } from "../SocketContext";
+import { Grid, Paper, Typography } from "@mui/material";
 
 const VideoPlayer = () => {
-  return (
-    <div>VideoPlayer</div>
-  )
-}
+	const { name, myVideo, userVideo, callAccepted, callEnded, stream, call } =
+		useContext(SocketContext);
 
-export default VideoPlayer
+	return (
+		<Grid
+			container
+			style={{
+				justifyContent: "center",
+			}}
+			flexDirection={{ xs: "column", sm: "row" }}
+		>
+			<Paper
+				style={{
+					padding: "10px",
+					border: "2px solid black",
+					margin: "10px",
+				}}
+			>
+				<Grid item xs={12} md={6}>
+					<Typography variant="h5" gutterBottom>
+						{name || "Your Name"}
+					</Typography>
+					<video
+						playsInline
+						muted
+						ref={myVideo}
+						autoPlay
+						style={{ width: "300px" }}
+					/>
+				</Grid>
+			</Paper>
+
+			{callAccepted && !callEnded ? (
+				<Paper
+					style={{
+						padding: "10px",
+						border: "2px solid black",
+						margin: "10px",
+					}}
+				>
+					<Grid item xs={12} md={6}>
+						<Typography variant="h5" gutterBottom>
+							{call.name || "Caller Name"}
+						</Typography>
+						<video
+							playsInline
+							ref={userVideo}
+							autoPlay
+							style={{ width: "500px" }}
+						/>
+					</Grid>
+				</Paper>
+			) : null}
+		</Grid>
+	);
+};
+
+export default VideoPlayer;
