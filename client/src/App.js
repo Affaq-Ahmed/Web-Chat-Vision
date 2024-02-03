3245fc068e7d9bf1bd81b3cacd784baae632b881
Affaq-Ahmed <aaawan165@gmail.com>
2024-02-03 14:14:39 +0500

    Add Notifications component to Options

--- client/src/App.js
+++ client/src/App.js
@@ -2,17 +2,40 @@ import React from "react";
 import { AppBar, Typography } from "@mui/material";
 import VideoPlayer from "./components/VideoPlayer";
 import Options from "./components/Options";
+import Notifications from "./components/Notifications";
 
 const App = () => {
 	return (
-		<div>
-			<AppBar position="static" color="inherit">
+		<div
+			style={{
+				display: "flex",
+				flexDirection: "column",
+				alignItems: "center",
+				width: "100%",
+			}}
+		>
+			<AppBar
+				position="static"
+				color="inherit"
+				style={{
+					borderRadius: 15,
+					margin: "30px 100px",
+					display: "flex",
+					flexDirection: "row",
+					justifyContent: "center",
+					alignItems: "center",
+					width: "600px",
+					border: "2px solid black",
+				}}
+			>
 				<Typography variant="h2" align="center">
 					Web Chat Vision
 				</Typography>
 			</AppBar>
 			<VideoPlayer />
-			<Options />
+			<Options>
+				<Notifications />
+			</Options>
 		</div>
 	);
 };
