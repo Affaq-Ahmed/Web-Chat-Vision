f26808d8e125d8f4bdfb83be574fa66fbcd1875f
Affaq-Ahmed <aaawan165@gmail.com>
2024-02-03 14:14:27 +0500

    Update ReactDOM import and render method

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { ContextProvider } from "./SocketContext";
 
-const rootElement = document.getElementById("root");
+const rootElement = ReactDOM.createRoot(document.getElementById("root"));
 
-ReactDOM.render(<App />, rootElement);
+rootElement.render(
+	<ContextProvider>
+		<App />
+	</ContextProvider>
+);
