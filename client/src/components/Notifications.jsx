927efa1838bc50f0cff4fd960be2e355b395241a
Affaq-Ahmed <aaawan165@gmail.com>
2024-02-03 17:47:55 +0500

    Add call notification functionality

--- client/src/components/Notifications.jsx
+++ client/src/components/Notifications.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { SocketContext } from "../SocketContext";
+import { Button } from "@mui/material";
 
 const Notifications = () => {
-  return (
-    <div>Notifications</div>
-  )
-}
+	const { answerCall, call, callAccepted } = useContext(SocketContext);
 
-export default Notifications
+	return (
+		<>
+			{call.isReceivingCall && !callAccepted && (
+				<div style={{ display: "flex", justifyContent: "space-around" }}>
+					<h1>{call.name} is calling:</h1>
+					<Button variant="contained" color="primary" onClick={answerCall}>
+						Answer
+					</Button>
+				</div>
+			)}
+		</>
+	);
+};
+
+export default Notifications;
